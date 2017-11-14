cdb23ebacef1fe63b4c0eb2e972fe4af5352b581
boB Rudis <bob@rud.is>
2017-11-14 17:37:04 -0500

    DESCRIPTION

--- R/exiv-package.R
+++ R/exiv-package.R
@@ -1,9 +1,8 @@
-#' ...
+#' Read and Write 'Exif' Image Tags
 #'
 #' @name exiv
 #' @docType package
 #' @author Bob Rudis (bob@@rud.is)
-#' @import purrr
 #' @useDynLib exiv
 #' @importFrom Rcpp sourceCpp
 NULL
